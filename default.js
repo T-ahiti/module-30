15809fb6b0934bafd35a2dbc934951a5bced5eca
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-17 18:44:49 +0600

    3rd commit

--- default.js
+++ default.js
@@ -1,1 +1,11 @@
 
+// default > when the valu is not provided.. 0 ekhane default dewa jay.. or jekno number chaile default dewa jabe
+
+function add (num1, num2=0){
+    const result = num1 + num2
+    console.log(num1, num2, result)
+    return result
+}
+
+// const sum = add(5,7)
+const sum = add(7)
