ad1a065648b0286a385f2a2df7bdcf2083b9f533
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-17 20:50:48 +0600

    third commit

--- arrow.js
+++ arrow.js
@@ -1,1 +1,17 @@
 
+// function declare
+function add(a, b)
+{return a + b}
+
+// function expression
+const add2 = function(a,b){
+    return a + b
+}
+
+// arrow function
+const add3 = (a,b) => a + b
+
+const sum = add(5,90)
+console.log(sum)
+
+
