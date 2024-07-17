06976da7c71eb079a6902123e0db090a5ab0a1ad
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-17 20:04:59 +0600

    arrow file added

---
+++ template.js
@@ -0,0 +1,13 @@
+
+
+const a = 10
+const b = 20
+const result = 'the sum of ' + a + ' and ' +  b  + ' is ' + (a+b)
+console.log(result)
+const math = `the sum of ${a} and ${b} is ${a + b}`
+
+const challange = `john john 
+cena cena lage
+mair dibo naki
+lets`
+console.log(challange)
